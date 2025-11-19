611f973a41a26c97e306b1b54400bbb28296d00a
MohamedYashar <arrafathyas@gmail.com>
2025-11-19 18:49:32 +0530

    V1 controllers/ api

---
+++ src/main/java/com/zosh/trading/controller/AssetController.java
@@ -0,0 +1,51 @@
+package com.zosh.trading.controller;
+
+import com.zosh.trading.model.Asset;
+import com.zosh.trading.model.User;
+import com.zosh.trading.service.AssetService;
+import com.zosh.trading.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class AssetController {
+
+    @Autowired
+    private  AssetService assetService;
+
+
+    @Autowired
+    private UserService userService;
+
+
+
+    @GetMapping("/{assetId}")
+    public ResponseEntity<Asset> getAssetById(@PathVariable Long assetId) throws Exception {
+        Asset asset = assetService.getAssetById(assetId);
+        return ResponseEntity.ok().body(asset);
+    }
+
+    @GetMapping("/coin/{coinId}/user")
+    public ResponseEntity<Asset> getAssetByUserIdAndCoinId(
+            @PathVariable String coinId,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+
+        User user=userService.findUserProfileByJwt(jwt);
+        Asset asset = assetService.findAssetByUserIdAndCoinId(user.getId(), coinId);
+        return ResponseEntity.ok().body(asset);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Asset>> getAssetsForUser(
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+        User user=userService.findUserProfileByJwt(jwt);
+        List<Asset> assets = assetService.getUsersAssets(user.getId());
+        return ResponseEntity.ok().body(assets);
+    }
+}
